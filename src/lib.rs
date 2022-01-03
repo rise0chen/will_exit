74139adad98442ff40b2c8e73605122026487047
rise0chen <rise0chen@163.com>
2022-01-03 11:50:26 +0800

    允许重复init

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ use core::time::Duration;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::{io, process, thread};
 
+static INITED: AtomicBool = AtomicBool::new(false);
 static EXITING: AtomicBool = AtomicBool::new(false);
 
 pub fn exit() {
@@ -21,12 +22,15 @@ pub fn will_exit() -> bool {
 }
 
 pub fn init() -> Result<(), io::Error> {
-    unsafe {
-        signal_hook_registry::register(libc::SIGINT, exit)?;
-        signal_hook_registry::register(libc::SIGTERM, exit)?;
-        signal_hook_registry::register(libc::SIGABRT, exit)?;
-        #[cfg(not(windows))]
-        signal_hook_registry::register(libc::SIGQUIT, exit)?;
-        Ok(())
+    let result = INITED.compare_exchange(false, true, Ordering::AcqRel, Ordering::Acquire);
+    if result.is_ok() {
+        unsafe {
+            signal_hook_registry::register(libc::SIGINT, exit)?;
+            signal_hook_registry::register(libc::SIGTERM, exit)?;
+            signal_hook_registry::register(libc::SIGABRT, exit)?;
+            #[cfg(not(windows))]
+            signal_hook_registry::register(libc::SIGQUIT, exit)?;
+        }
     }
+    Ok(())
 }
