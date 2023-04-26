5080d61b419943ed7eda300cc067cf49caf668a5
rise0chen <rise0chen@163.com>
2023-04-26 13:57:40 +0800

    feat: block exit

--- examples/base.rs
+++ examples/base.rs
@@ -5,7 +5,7 @@ fn main() {
     will_exit::init(2000).unwrap();
     thread::spawn(|| {
         thread::sleep(Duration::from_secs(6));
-        will_exit::exit();
+        will_exit::exit(0);
     });
     loop {
         if will_exit::will_exit() {
--- src/lib.rs
+++ src/lib.rs
@@ -8,28 +8,31 @@ static EXIT_TIME: AtomicU16 = AtomicU16::new(0);
 #[cfg(feature = "async")]
 static EVENT: event_listener::Event = event_listener::Event::new();
 
-pub fn exit() {
-    let result = EXITING.compare_exchange(false, true, Ordering::AcqRel, Ordering::Acquire);
-    if result.is_ok() {
-        #[cfg(feature = "async")]
-        EVENT.notify(usize::MAX);
-        thread::Builder::new()
-            .name("will_exit".to_string())
-            .spawn(|| {
-                let exit_time = EXIT_TIME.load(Ordering::SeqCst);
-                thread::sleep(Duration::from_millis(exit_time as u64));
-                process::exit(0);
-            })
-            .unwrap();
-    }
+pub fn exit(code: i32) -> ! {
+    EXITING.store(true, Ordering::SeqCst);
+    #[cfg(feature = "async")]
+    EVENT.notify(usize::MAX);
+
+    let exit_time = EXIT_TIME.load(Ordering::SeqCst);
+    thread::sleep(Duration::from_millis(exit_time as u64));
+    process::exit(code);
 }
+pub fn send_exit_signal(code: i32) {
+    thread::Builder::new()
+        .name("will_exit".to_string())
+        .spawn(move || exit(code))
+        .unwrap();
+}
+
 pub fn will_exit() -> bool {
     EXITING.load(Ordering::Acquire)
 }
 #[cfg(feature = "async")]
 #[cfg_attr(docsrs, doc(cfg(feature = "async")))]
 pub async fn wait_will_exit() {
-    EVENT.listen().await
+    while EXITING.load(Ordering::Acquire) == false {
+        EVENT.listen().await
+    }
 }
 
 /// 设置调用exit()后多少毫秒后退出程序
@@ -38,11 +41,11 @@ pub fn init(exit_time: u16) -> Result<(), io::Error> {
     let result = INITED.compare_exchange(false, true, Ordering::AcqRel, Ordering::Acquire);
     if result.is_ok() {
         unsafe {
-            signal_hook_registry::register(libc::SIGINT, exit)?;
-            signal_hook_registry::register(libc::SIGTERM, exit)?;
-            signal_hook_registry::register(libc::SIGABRT, exit)?;
+            signal_hook_registry::register(libc::SIGINT, || exit(libc::SIGINT))?;
+            signal_hook_registry::register(libc::SIGTERM, || exit(libc::SIGTERM))?;
+            signal_hook_registry::register(libc::SIGABRT, || exit(libc::SIGABRT))?;
             #[cfg(not(windows))]
-            signal_hook_registry::register(libc::SIGQUIT, exit)?;
+            signal_hook_registry::register(libc::SIGQUIT, || exit(libc::SIGQUIT))?;
         }
     }
     Ok(())
